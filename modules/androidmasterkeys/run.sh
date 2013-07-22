120b09db7c5503dc42d8af31e14b100ae59554f0
AFE <security@xysec.com>
2013-07-22 15:14:33 +0530

    Updated Copyright Notice
    Updated Copyright Notice

--- modules/androidmasterkeys/run.sh
+++ modules/androidmasterkeys/run.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #----------------------------------------------------------------------------------------------#
 #Android Framework for Exploitation v-1                                                        #
-# (C)opyright 2010 - XYS3C                                                                     #
+# (C)opyright 2012 - XYS3C                                                                     #
 #---Important----------------------------------------------------------------------------------#
 #                     *** Do NOT use this for illegal or malicious use ***                     #
 #              The programs are provided as is without any guarantees or warranty.             #
